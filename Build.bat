ed631219c4179acde873a8cd326c4bf49f52af97
Cornelius Andreas Rosenaa <macrosedk@gmail.com>
2024-07-30 00:56:55 +0200

    Reconfigure build system

--- Build.bat
+++ Build.bat
@@ -1,6 +1,6 @@
-python -m venv .venv
+python -m venv .venv\win
 
-call .venv\Scripts\activate.bat
+call .venv\win\Scripts\activate.bat
 
 python -m pip install -r requirements.txt
 python -m pip install -U pyinstaller
@@ -9,7 +9,7 @@ python -m black FOLON-Downgrader.py
 
 python -m PyInstaller --clean -y "FOLON-Downgrader.spec"
 
-call .venv\Scripts\deactivate.bat
+call .venv\win\Scripts\deactivate.bat
 
 echo "https://gist.github.com/PaulCreusy/7fade8d5a8026f2228a97d31343b335e"
 echo "signtool sign /f ./certificate.pfx /p <password> /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 .\dist\FOLON-Downgrader.exe"
