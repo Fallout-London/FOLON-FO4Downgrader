c54ec960fd85e8237094dcbaa393a9f188e59e74
Cornelius Andreas Rosenaa <macrosedk@gmail.com>
2024-08-02 11:57:14 +0200

    Oops ;

--- Run.sh
+++ Run.sh
@@ -5,6 +5,6 @@ source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
 env QT_QPA_PLATFORM=xcb WAYLAND_DISPLAY= python3 ./FOLON-Downgrader.py
-if [ $? = 134] then
+if [ $? = 134]; then
     echo "Please check dependencies from here https://developer.valvesoftware.com/wiki/SteamCMD#Manually and if libgl is installed"
 fi
