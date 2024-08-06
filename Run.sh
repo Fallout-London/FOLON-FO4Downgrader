036695e576ada3d44a3ec3d6ebf505bc70b61033
Cornelius Andreas Rosenaa <macrosedk@gmail.com>
2024-08-06 18:24:28 +0200

    Add Linux specific requirements

--- Run.sh
+++ Run.sh
@@ -2,7 +2,7 @@ python3 -m venv .venv
 
 source .venv/bin/activate
 
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements-linux.txt
 
 if ! env QT_QPA_PLATFORM=xcb WAYLAND_DISPLAY= python3 ./FOLON-Downgrader.py; then
     echo "Please check dependencies from here https://developer.valvesoftware.com/wiki/SteamCMD#Manually and if libgl is installed"
