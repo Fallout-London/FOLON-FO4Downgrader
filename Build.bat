3870dfe77d8e9e94f01a146bafc5a5b3cfe81afd
Cornelius Andreas Rosenaa <macrosedk@gmail.com>
2024-08-13 18:53:04 +0200

    I just like it this way

--- Build.bat
+++ Build.bat
@@ -13,3 +13,5 @@ call .venv\win\Scripts\deactivate.bat
 
 echo "https://gist.github.com/PaulCreusy/7fade8d5a8026f2228a97d31343b335e"
 echo "signtool sign /f ./certificate.pfx /p <password> /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 .\dist\FOLON-Downgrader.exe"
+
+start dist
